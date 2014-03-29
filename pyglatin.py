b500fe0d0afd30bfddfc29c2d1f869c539ef6515
Kahli Burke <kahli.burke@wintermutesys.com>
2014-03-29 16:25:41 -0700

    Move pyglatin to its own file

---
+++ pyglatin.py
@@ -0,0 +1,14 @@
+__author__ = 'bjorn'
+
+pyg = 'ay'
+
+original = raw_input('Enter a word: ')
+
+if len(original) > 0 and original.isalpha():
+    print original
+    word=original.lower()
+    first=word[0]
+    new_word=word[1:]+first+pyg
+    print new_word
+else:
+    print 'empty'
