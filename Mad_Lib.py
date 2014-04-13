6b611c25e92620c9d71b7c5d26e3945b532ce03e
Kahli Burke <kahli.burke@wintermutesys.com>
2014-04-13 14:59:31 -0700

    Add file parsing

--- Mad_Lib.py
+++ Mad_Lib.py
@@ -1,8 +1,9 @@
 __author__ = 'Bjorn'
 
 import os
+import re
 
-debug = False
+debug = True
 
 def list_stories():
     return os.listdir("stories")
@@ -13,6 +14,32 @@ def choose_story():
         print str(idx+1)+": "+files[idx]
     story = int(raw_input("Which story do you want? "))
     return files[story-1]
-print choose_story()
-if (debug):
-    print list_stories()
+
+story_file = choose_story()
+
+if debug:
+    print "You chose: "+story_file
+
+file = open("stories/"+story_file, "r")
+story = file.read()
+file.close()
+
+words_to_replace = re.findall( "_(.*?)_", story)
+
+word_map = {
+    "NOUN": "a noun (object or thing)",
+    "PRONOUN": "a singular pronoun (he, she)",
+    "VERB-PRESENT": "a verb, present tense",
+    "VERB-PAST": "a verb, past tense",
+    "ADJECTIVE": "an adjective",
+    "NAME": "someone's name",
+    "PART-OF-BODY": "a body part",
+    "VERB-FUTURE": "a verb, future tense"
+}
+
+if debug:
+    print words_to_replace
+
+if debug:
+    print story
+
